7e00cd8f2b13b0bf62a4fac764badb148b51ef77
adrianjg <adrianjimenezgalan@gmail.com>
2022-05-24 23:19:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Tiled to GBDK C File export
+This is a extension/export plugin for the [Tiled map editor](https://www.mapeditor.org/) that adds the following type to the "Export As" menu:
+
+* GBDK C file (*.c, *.h)
+
+This save option generates a tile array that can be used with GBDK's background and window functions such as set_bkg_tiles or set_win_tiles.
+
+<sub>* Blank tiles are defaulted to 0x00</sub>
+
+This extension is based on [Tiled to GBA export](https://github.com/djedditt/tiled-to-gba-export).
+
+## Installation
+This extension requires Tiled 1.4 or newer. Get the latest version [here](https://www.mapeditor.org/).
+
+To add this extension to your Tiled installation:
+* Open Tiled and go to Edit > Preferences > Plugins and click the "Open" button to open the extensions directory.
+* Download [tiled-to-gbdk-c-file-export.js](https://raw.githubusercontent.com/ajgalan/tiled-to-gbdk-c-file-export/main/tiled-to-gbdk-c-file-export.js) in this repository and copy it to that location. The script can be placed either directly in the extensions directory or in a subdirectory.
+
+## Output
+The export plugin generates a .c source file and associated .h header file.
+
+**example.h**
+
+```C
+#ifndef __example_h_INCLUDE
+#define __example_h_INCLUDE
+
+#define exampleWidth 64
+#define exampleHeight 64
+
+extern const unsigned char example[];
+
+#endif
+
+
+```
+
+**example.c**
+
+```C
+#include "example.h"
+
+const unsigned char example[] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+    0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13,
+    0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
+    etc.
+};
+
+
+
+```
+
+## License
+This work is licensed under the MIT License. See [LICENSE](https://raw.githubusercontent.com/ajgalan/tiled-to-gbdk-c-file-export/main/LICENSE) for details.
---
+++ tiled-to-gbdk-c-file-export.js
@@ -0,0 +1,127 @@
+/*
+ * tiled-to-gbdk-c-file-export.js
+ *
+ * This extension adds the "GBDK C file" type to the "Export As" menu,
+ * which generates a tile array that can be used with GBDK's background and
+ * window functions such as set_bkg_tiles or set_win_tiles.
+ * 
+ * This extension is based on "Tiled to GBA export" by Jay van Hutten
+ * https://github.com/djedditt/tiled-to-gba-export
+ *
+ * The first tile layer found is converted to a C array of hexadecimal tile
+ * IDs casted into an unsigned 8 bit integer. Blank tiles are defaulted to 0x00.
+ *
+ * Copyright (c) 2020 Jay van Hutten
+ * Copyright (c) 2016 Adrián Jiménez Galán
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+var tiledToGbdkCFileExport = {
+    name: "GBDK C file",
+    extension: "c *.h",
+    write:
+
+    function(p_map, p_fileName) {
+        console.time("Export completed in");
+
+        // Split full filename path into the filename (without extension) and the directory
+        let fileBaseName = FileInfo.completeBaseName(p_fileName).replace(/[^a-zA-Z0-9-_]/g, "_");
+        let filePath = FileInfo.path(p_fileName) + "/";
+        
+        // Replace the ‘/’ characters in the file path for ‘\’ on Windows
+        filePath = FileInfo.toNativeSeparators(filePath);
+
+        //Generate header file data
+        let headerFileData = "";
+        headerFileData += "#ifndef __" + fileBaseName + "_h_INCLUDE\n";
+        headerFileData += "#define __" + fileBaseName + "_h_INCLUDE\n\n";
+        headerFileData += "#define " + fileBaseName +"Width " + p_map.width + "\n";
+        headerFileData += "#define " + fileBaseName + "Height " + p_map.height + "\n\n";
+
+        headerFileData += "extern const unsigned char "+ fileBaseName + "[];\n\n";
+        headerFileData += "#endif\n";
+
+        //Generate tilemap file data
+        let tilemapFileData = "";
+        tilemapFileData += "#include \"" + fileBaseName + ".h\"\n\n";
+        tilemapFileData += "const unsigned char " + fileBaseName + "[] = {";
+
+        //Find first tile layer
+        let tileLayerIndex = -1;
+        let currentLayer;
+
+        for (let i = 0; i < p_map.layerCount; ++i) {
+            currentLayer = p_map.layerAt(i);
+            if (currentLayer.isTileLayer) {
+                tileLayerIndex  = i;
+                break;
+            }
+        }
+        //If no tile layer is found, throw error.
+        if (tileLayerIndex == -1) {
+            return "Export failed: No Tile Layer found."
+        }
+
+        let i = 0;
+
+        for (let y = 0; y < currentLayer.height; y++) {
+            for (let x = 0; x < currentLayer.width; x++) {
+                let currentTile = currentLayer.cellAt(x, y);
+                
+                //Write a line break each 10 tiles
+                if (i++ % 10 == 0) {
+                    tilemapFileData += "\n    ";
+                }                
+
+                //If not defined, write tile 0
+                if (currentTile.tileId == "-1") {
+                    tilemapFileData += "0x00, ";
+                } else {
+                    //Cast number id into an unsigned 8 bit integer
+                    let tileId = currentTile.tileId % 256;
+                    
+                    //Write tileId as hexadecimal
+                    let value = '0x' + tileId.toString(16).padStart(2, "0");          
+                    tilemapFileData += value + ", ";
+                }
+            }
+        }
+
+        // Remove the last comma and close the array.
+        tilemapFileData = tilemapFileData.slice(0, -2)+"\n};\n";
+
+        // Write header data to disk
+        let headerFile = new TextFile(filePath + fileBaseName + ".h", TextFile.WriteOnly);
+        headerFile.write(headerFileData);
+        headerFile.commit();
+        console.log("Header file exported to " + filePath+fileBaseName + ".h");
+
+        // Write source data to disk
+        let tilemapFile = new TextFile(filePath + fileBaseName + ".c", TextFile.WriteOnly);
+        tilemapFile.write(tilemapFileData);
+        tilemapFile.commit();
+        console.log("Tilemap file exported to " + filePath + fileBaseName + ".c");
+
+        console.timeEnd("Export completed in");
+    }
+}
+
+tiled.registerMapFormat("TiledToGbdkCFileExport", tiledToGbdkCFileExport)
